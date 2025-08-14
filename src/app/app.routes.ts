33ae7b73fcaa89e4633c02f66f9d0da09ea9c16e
doannguyen <doannguyen@safeforweb.com>
2025-08-14 13:55:42 +0700

    - add layout component and routing - add environment config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    data: {
+      layout: 'empty'
+    },
+    children: [
+      { path: 'sign-in', loadChildren: () => import('./modules/auth/sign-in/sign-in.routes') },
+      { path: 'sign-up', loadChildren: () => import('./modules/auth/sign-up/sign-up.routes') }
+    ]
+  },
+  {
+        path: '',
+        component: LayoutComponent,
+        children: [
+          { path: 'home', loadChildren: () => import('./modules/landing/home/home.routes') },
+          { path: 'pokemon', loadChildren: () => import('./modules/landing/pokemon/pokemon.routes') }
+        ]
+    }
+];
---
+++ src/app/modules/landing/pokemon/pokemon.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+export default [
+    {
+        path: '',
+        component: ListComponent,
+    },
+] as Routes;
